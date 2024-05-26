60b235bba0e332df70fa936874e982772bf1d269
grtsinry43 <grtsinry43@outlook.com>
2024-05-26 08:51:57 +0800

    创建项目，引入element-plus

---
+++ src/components/Banner.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="banner-container">
+    <span class="title">简单售票系统</span>
+    <ul class="banner-item-container">
+      <li class="banner-item"></li>
+    </ul>
+    <ul class="banner-profile-container">
+      <li class="banner-profile-username"></li>
+      <li class="banner-profile-action">
+        <button>登录</button>
+        <button>退出登录</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  HomeView
+</template>
+
+<style scoped>
+
+</style>
